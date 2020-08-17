3c578aa7f9756521f5eba383b19b37dd95f073a8
shaikhov-i <65728594+shaikhov-i@users.noreply.github.com>
2020-08-17 15:49:50 +0500

    Creating snake

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+var blockSize = 10;
+var widthInBlocks = width / blockSize;
+var heightInBlocks = height / blockSize;
+var score = 0;
+
+var drawBorder = function() {
+  ctx.fillStyle = "Gray";
+  ctx.fillRect(0, 0, width, blockSize);
+  ctx.fillRect(0, height - blockSize, width, blockSize);
+  ctx.fillRect(0, 0, blockSize, height);
+  ctx.fillRect(width - blockSize, 0, blockSize, height);
+  ctx.fillText("Привет, мир!", 50, 50);
+}
+
+drawBorder();
